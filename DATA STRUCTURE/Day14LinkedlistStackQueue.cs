0508cba42da60b5687ec658527f8c89d414a1bd2
Darshan Thorat <darshaanthorat17@gmail.com>
2022-02-21 12:48:11 +0530

    LinkListProb

---
+++ DATA STRUCTURE/Day14LinkedlistStackQueue.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA_STRUCTURE
+{
+    internal class Day14LinkedlistStackQueue
+    {
+        internal Node head; //new 
+        private Node top;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack ", this.top.data);//NullrefrenceRxception
+        }
+
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+        class LinkedListQueue
+        {
+            Node head = null;
+
+            internal void Enqueue(int data)
+            {
+                Node node = new Node(data);
+                if (head == null)
+                    head = node;
+                else
+                {
+                    Node temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                    node.next = head;
+
+                }
+                Console.WriteLine("{0} inserted into queue ", node.data);
+
+            }
+
+            internal void Dequeue()
+            {
+                Node temp = this.head;
+                if (temp.next == null)
+                {
+                    this.head = null;
+                }
+                while (temp.next != null)
+                {
+                    if (temp.next.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    else
+                    {
+                        temp.next = null;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StackUsingLinklist/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace DATA_STRUCTURE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Queue Using Linked List");
+            LinkedListUsingQueue linkedListQueue = new LinkedListUsingQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            Console.WriteLine("The Queue is :");
+            linkedListQueue.Display();
+            Console.WriteLine("\n---------------------\n");
+            Console.WriteLine("Performing Dequeue Operation till queue got empty.....");
+            linkedListQueue.Dequeue();
+            linkedListQueue.Display();
+            Console.WriteLine("\n---------------------\n");
+            linkedListQueue.Dequeue();
+            linkedListQueue.Display();
+            Console.WriteLine("\n---------------------\n");
+            linkedListQueue.Dequeue();
+            linkedListQueue.Display();
+            Console.ReadKey();
+
+        }
+
+    }
+}
